e56320e409d3e93e2b30e838acdf8a32cb47b761
klaus01 <kelei0017@gmail.com>
2015-04-06 15:29:43 +0800

    增加amrToWav、wavToAmr方法和相关文件

---
+++ appWEI/appWEI-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  appWEI-Bridging-Header.h
+//  appWEI
+//
+//  Created by kelei on 15/4/6.
+//  Copyright (c) 2015年 kelei. All rights reserved.
+//
+
+#ifndef appWEI_appWEI_Bridging_Header_h
+#define appWEI_appWEI_Bridging_Header_h
+
+//#import "VoiceConverter.h"
+
+#endif
