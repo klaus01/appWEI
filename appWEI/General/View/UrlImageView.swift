58d3bc46ceb3e561758858a3cb6b56110f057f20
klaus01 <kelei0017@gmail.com>
2015-04-16 22:47:26 +0800

    iOS7下不能使用Kingfisher，只有先不用，如果以后不需要支持iOS7了再使用

--- appWEI/General/View/UrlImageView.swift
+++ appWEI/General/View/UrlImageView.swift
@@ -10,18 +10,55 @@ import UIKit
 
 class UrlImageView: UIImageView {
 
-    private var imageTask: RetrieveImageTask?
-    
+    private var _imageUrl: String?
     var imageUrl: String? {
-        didSet {
-            imageTask?.cancel()
-            if let url = imageUrl {
-                imageTask = self.kf_setImageWithURL(NSURL(string: url)!, placeholderImage: UIImage(named: "imagePlaceholder"))
-            }
-            else {
-                imageTask = nil
+        get {
+            return _imageUrl
+        }
+        set {
+            if _imageUrl != newValue {
+                _imageUrl = newValue
+                if let _imageUrl = _imageUrl {
+                    let fileName = (_imageUrl as NSString).lastPathComponent
+                    let filePath = getCachesDirectory() + "/" + fileName
+                    if NSFileManager.defaultManager().fileExistsAtPath(filePath) {
+                        self.image = UIImage(contentsOfFile: filePath)
+                    }
+                    else {
+                        let hud = JHProgressHUD()
+                        hud.backGroundColor = UIColor.whiteColor()
+                        hud.loaderColor = UIColor.blackColor()
+                        hud.showInView(self)
+                        download(Method.GET, _imageUrl, { (temporaryURL, res) -> (NSURL) in
+                            return NSURL(string: "file://" + filePath)!
+                        }).response { (request, response, _, error) in
+                            // errorCode 516: 下载后保存的目标文件已经存在(同一文件下载多次时出现)
+                            if error != nil && error!.code != 516 {
+                                println(error)
+                                return
+                            }
+                            hud.hide()
+                            self.image = UIImage(contentsOfFile: filePath)
+                        }
+                    }
+                }
             }
         }
     }
+    
+    // 8.0及以上才能使用Kingfisher
+//    private var imageTask: RetrieveImageTask?
+//
+//    var imageUrl: String? {
+//        didSet {
+//            imageTask?.cancel()
+//            if let url = imageUrl {
+//                imageTask = self.kf_setImageWithURL(NSURL(string: url)!, placeholderImage: UIImage(named: "imagePlaceholder"))
+//            }
+//            else {
+//                imageTask = nil
+//            }
+//        }
+//    }
     
 }
