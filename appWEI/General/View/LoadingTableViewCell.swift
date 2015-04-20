bd1a0a12bf8f73804d5207ea12f5bc4d24df1745
klaus01 <kelei0017@gmail.com>
2015-04-21 00:29:14 +0800

    增加LoadingTableViewCell

---
+++ appWEI/General/View/LoadingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoadingTableViewCell.swift
+//  appWEI
+//
+//  Created by kelei on 15/4/20.
+//  Copyright (c) 2015年 kelei. All rights reserved.
+//
+
+import UIKit
+
+class LoadingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- appWEI/Sections/DictionaryViewController.swift
+++ appWEI/Sections/DictionaryViewController.swift
@@ -12,7 +12,11 @@ class DictionaryViewController: UIViewController {
 
     private let pageCount = 20
     private var words = [WordModel]()
+    private var allLoaded = false
     
+    private func getWordCount() -> Int {
+        return words.count + (allLoaded ? 0 : 1)
+    }
     private func loadMoreWords() {
         ServerHelper.wordFindByAppUser(offset: words.count, resultCount: pageCount) { [weak self] (ret, error) -> Void in
             if let error = error {
@@ -21,7 +25,12 @@ class DictionaryViewController: UIViewController {
             }
             if ret!.success {
                 if let weakSelf = self, let data = ret!.data {
-                    self!.words += data
+                    if data.count > 0 {
+                        self!.words += data
+                    }
+                    else {
+                        self!.allLoaded = true
+                    }
                     self!.tableView.reloadData()
                 }
             }
@@ -38,29 +47,36 @@ class DictionaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        loadMoreWords()
-        
         let cellNib = UINib(nibName: "WordTableViewCell", bundle: nil)
         tableView.registerNib(cellNib, forCellReuseIdentifier: "MYCELL")
+        let loadingCellNib = UINib(nibName: "LoadingTableViewCell", bundle: nil)
+        tableView.registerNib(loadingCellNib, forCellReuseIdentifier: "LOADINGCELL")
         tableView.ce_NumberOfRowsInSection { [weak self] (tableView, section) -> Int in
-            return self!.words.count
+            return self!.getWordCount()
         }
         .ce_CellForRowAtIndexPath { [weak self] (tableView, indexPath) -> UITableViewCell in
-            let cell = tableView.dequeueReusableCellWithIdentifier("MYCELL", forIndexPath: indexPath) as! WordTableViewCell
-            let word = self!.words[indexPath.row]
-            
-            cell.number = word.number
-            cell.pictureImageUrl = word.pictureUrl
-            cell.rightText = word.description
-            
-            return cell
+            if !(self!.allLoaded) && self!.getWordCount() == (indexPath.row + 1) {
+                return tableView.dequeueReusableCellWithIdentifier("LOADINGCELL", forIndexPath: indexPath) as! UITableViewCell
+            }
+            else {
+                let cell = tableView.dequeueReusableCellWithIdentifier("MYCELL", forIndexPath: indexPath) as! WordTableViewCell
+                let word = self!.words[indexPath.row]
+                
+                cell.number = word.number
+                cell.pictureImageUrl = word.pictureUrl
+                cell.rightText = word.description
+                
+                return cell
+            }
         }
         .ce_DidSelectRowAtIndexPath { [weak self] (tableView, indexPath) -> Void in
-            println("选中了\(indexPath)")
-            // TODO 进入字界面
+            if (self!.allLoaded) || self!.getWordCount() != (indexPath.row + 1) {
+                println("选中了\(indexPath)")
+                // TODO 进入字界面
+            }
         }
         .ce_WillDisplayCell { [weak self] (tableView, cell, indexPath) -> Void in
-            if self!.words.count == (indexPath.row + 1) {
+            if self!.getWordCount() == (indexPath.row + 1) {
                 self!.loadMoreWords()
             }
         }
