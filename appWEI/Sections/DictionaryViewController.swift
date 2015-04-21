f9bdaf8eedc1beabfe6637257856bc03ae2dc1e6
klaus01 <kelei0017@gmail.com>
2015-04-21 22:26:50 +0800

    完成“典”界面的查询功能

--- appWEI/Sections/DictionaryViewController.swift
+++ appWEI/Sections/DictionaryViewController.swift
@@ -10,15 +10,85 @@ import UIKit
 
 class DictionaryViewController: UIViewController {
 
+    enum SearchType {
+        case Number
+        case Description
+    }
     private let pageCount = 20
     private var words = [WordModel]()
     private var allLoaded = false
+    private var searchType = SearchType.Number
+    
+    @IBOutlet weak var searchTypeButton: UIButton!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupSearchTypeButton()
+        self.setupSearchTextField()
+        self.setupTableView()
+    }
+    
+    private func setupSearchTypeButton() {
+        searchTypeButton.clicked { [weak self] (button) -> Void in
+            self!.searchType = self!.searchType == .Number ? .Description : .Number
+            button.setTitle(self!.searchType == .Number ? "编号" : "描述", forState: UIControlState.Normal)
+        }
+    }
+    
+    private func setupSearchTextField() {
+        searchTextField.ce_ShouldReturn { [weak self] (textField) -> Bool in
+            self!.words.removeAll(keepCapacity: false)
+            self!.allLoaded = false
+            self!.tableView.reloadData()
+            textField.resignFirstResponder()
+            return true
+        }
+    }
+    
+    private func setupTableView() {
+        let cellNib = UINib(nibName: "WordTableViewCell", bundle: nil)
+        tableView.registerNib(cellNib, forCellReuseIdentifier: "MYCELL")
+        let loadingCellNib = UINib(nibName: "LoadingTableViewCell", bundle: nil)
+        tableView.registerNib(loadingCellNib, forCellReuseIdentifier: "LOADINGCELL")
+        tableView.ce_NumberOfRowsInSection { [weak self] (tableView, section) -> Int in
+            return self!.getWordCount()
+            }
+            .ce_CellForRowAtIndexPath { [weak self] (tableView, indexPath) -> UITableViewCell in
+                if !(self!.allLoaded) && self!.getWordCount() == (indexPath.row + 1) {
+                    return tableView.dequeueReusableCellWithIdentifier("LOADINGCELL", forIndexPath: indexPath) as! UITableViewCell
+                }
+                else {
+                    let cell = tableView.dequeueReusableCellWithIdentifier("MYCELL", forIndexPath: indexPath) as! WordTableViewCell
+                    let word = self!.words[indexPath.row]
+                    
+                    cell.number = word.number
+                    cell.pictureImageUrl = word.pictureUrl
+                    cell.rightText = word.description
+                    
+                    return cell
+                }
+            }
+            .ce_DidSelectRowAtIndexPath { [weak self] (tableView, indexPath) -> Void in
+                if (self!.allLoaded) || self!.getWordCount() != (indexPath.row + 1) {
+                    println("选中了\(indexPath)")
+                    // TODO 进入字界面
+                }
+            }
+            .ce_WillDisplayCell { [weak self] (tableView, cell, indexPath) -> Void in
+                if self!.getWordCount() == (indexPath.row + 1) {
+                    self!.loadMoreWords()
+                }
+        }
+    }
     
     private func getWordCount() -> Int {
         return words.count + (allLoaded ? 0 : 1)
     }
+    
     private func loadMoreWords() {
-        ServerHelper.wordFindByAppUser(offset: words.count, resultCount: pageCount) { [weak self] (ret, error) -> Void in
+        let completionHandler = { [weak self] (ret: ServerResultModel<[WordModel]>?, error: NSError?) -> Void in
             if let error = error {
                 println(error)
                 return
@@ -38,58 +108,15 @@ class DictionaryViewController: UIViewController {
                 UIAlertView.showMessage(ret!.errorMessage!)
             }
         }
+        if searchTextField.text == nil || searchTextField.text!.length <= 0 {
+            ServerHelper.wordFindByAppUser(offset: words.count, resultCount: pageCount, completionHandler: completionHandler)
+        }
+        else if searchType == .Number {
+            ServerHelper.wordFindByAppUser(number: searchTextField.text!, offset: words.count, resultCount: pageCount, completionHandler: completionHandler)
+        }
+        else {
+            ServerHelper.wordFindByAppUser(description: searchTextField.text!, offset: words.count, resultCount: pageCount, completionHandler: completionHandler)
+        }
     }
     
-    @IBOutlet weak var searchTypeButton: UIButton!
-    @IBOutlet weak var searchTextField: UITextField!
-    @IBOutlet weak var tableView: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let cellNib = UINib(nibName: "WordTableViewCell", bundle: nil)
-        tableView.registerNib(cellNib, forCellReuseIdentifier: "MYCELL")
-        let loadingCellNib = UINib(nibName: "LoadingTableViewCell", bundle: nil)
-        tableView.registerNib(loadingCellNib, forCellReuseIdentifier: "LOADINGCELL")
-        tableView.ce_NumberOfRowsInSection { [weak self] (tableView, section) -> Int in
-            return self!.getWordCount()
-        }
-        .ce_CellForRowAtIndexPath { [weak self] (tableView, indexPath) -> UITableViewCell in
-            if !(self!.allLoaded) && self!.getWordCount() == (indexPath.row + 1) {
-                return tableView.dequeueReusableCellWithIdentifier("LOADINGCELL", forIndexPath: indexPath) as! UITableViewCell
-            }
-            else {
-                let cell = tableView.dequeueReusableCellWithIdentifier("MYCELL", forIndexPath: indexPath) as! WordTableViewCell
-                let word = self!.words[indexPath.row]
-                
-                cell.number = word.number
-                cell.pictureImageUrl = word.pictureUrl
-                cell.rightText = word.description
-                
-                return cell
-            }
-        }
-        .ce_DidSelectRowAtIndexPath { [weak self] (tableView, indexPath) -> Void in
-            if (self!.allLoaded) || self!.getWordCount() != (indexPath.row + 1) {
-                println("选中了\(indexPath)")
-                // TODO 进入字界面
-            }
-        }
-        .ce_WillDisplayCell { [weak self] (tableView, cell, indexPath) -> Void in
-            if self!.getWordCount() == (indexPath.row + 1) {
-                self!.loadMoreWords()
-            }
-        }
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
