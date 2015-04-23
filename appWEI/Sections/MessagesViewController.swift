48ec9a31dc43cfd36cd5901b749f7b06da4a7abd
klaus01 <kelei0017@gmail.com>
2015-04-23 23:08:27 +0800

    完成点击未出消息后展示；完成点击消息图片长按两秒事件

--- appWEI/Sections/MessagesViewController.swift
+++ appWEI/Sections/MessagesViewController.swift
@@ -33,21 +33,103 @@ private func setUnreadMessageToImage(message: UnreadMessageModel, imageView: UII
     }
 }
 
+class WordView: UIView {
+    
+    private var isAnimation = false
+    
+    @IBOutlet weak var wordImageView: UIImageView!
+    @IBOutlet weak var wordMaskView: UIView!
+    @IBOutlet weak var wordButton: UIButton!
+    
+    private var _longPressAction: (() -> ())?
+    
+    var message: UnreadMessageModel! {
+        didSet {
+            setUnreadMessageToImage(message, wordImageView, 10)
+            setupWordMaskView()
+            setupWordButton()
+        }
+    }
+    
+    private func setupWordMaskView() {
+        var frame = self.wordMaskView.superview!.bounds
+        frame.origin.y = frame.size.height
+        frame.size.height = 0
+        self.wordMaskView.frame = frame
+        self.wordMaskView.alpha = 0.3
+    }
+    
+    private func setupWordButton() {
+        wordButton.hidden = message.message.type != .Word
+        wordButton.__on(UIControlEvents.TouchDown) { [weak self] (control) -> () in
+            self!.setupWordMaskView()
+            self!.isAnimation = true
+            UIView.setAnimationsEnabled(true)
+            UIView.animateWithDuration(2.0, animations: { [weak self] () -> Void in
+                self!.wordMaskView.frame = self!.wordMaskView.superview!.bounds
+                }, completion: { [weak self] (success) -> Void in
+                    self!.isAnimation = false
+                    if success {
+                        UIView.animateWithDuration(0.3, animations: { [weak self] () -> Void in
+                            self!.wordMaskView.alpha = 0
+                            })
+                        if let f = self!._longPressAction {
+                            f()
+                        }
+                    }
+                })
+        }
+        let touchUpAction = { [weak self] (control: UIControl) -> () in
+            if self!.isAnimation {
+                self!.setupWordMaskView()
+                self!.wordMaskView.layer.removeAllAnimations()
+            }
+        }
+        wordButton.__on(UIControlEvents.TouchUpInside, action: touchUpAction)
+        wordButton.__on(UIControlEvents.TouchUpOutside, action: touchUpAction)
+    }
+    
+    func longPressAction(action: () -> ()) {
+        _longPressAction = action
+    }
+}
+
 class MessagesViewController: UIViewController {
 
-    @IBOutlet weak var wordImageView: UIImageView!
-    @IBOutlet weak var wordSendTimeLabel: UILabel!
+    private var unreadMessages: [UnreadMessageModel]!
+    private var currentDisplayMessage: UnreadMessageModel?
+    
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var wordView: WordView!
     @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var wordSendTimeLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupCollectionView()
+        wordView.hidden = true
+        wordSendTimeLabel.hidden = true
+        playButton.hidden = true
+        forwardButton.hidden = true
+        playButton.clicked() { (UIButton) -> () in
+            // TODO 播放音频
+        }
+        wordView.longPressAction { () -> () in
+            // TODO 原消息回复
+        }
+        forwardButton.clicked() { (UIButton) -> () in
+            // TODO 转发消息
+        }
+        
+        reloadUnreadMessages()
+        ce_addObserverForName(kNotification_UpdateUnreadMessagesComplete) { [weak self] (notification) -> Void in
+            self!.reloadUnreadMessages()
+        }
     }
 
-    func setupCollectionView() {
+    private func setupCollectionView() {
         
         let ROW_COUNT = 6.0
         let CELL_WIDTH = 50.0
@@ -56,21 +138,19 @@ class MessagesViewController: UIViewController {
         let cellNib = UINib(nibName: "ImageCollectionViewCell", bundle: nil)
         collectionView.registerNib(cellNib, forCellWithReuseIdentifier: "MYCELL")
         collectionView
-        .ce_NumberOfItemsInSection { (collectionView, section) -> Int in
-            return UserInfo.shared.unreadMessages.count
-//            return 10
+        .ce_NumberOfItemsInSection { [weak self] (collectionView, section) -> Int in
+            return self!.unreadMessages.count
         }
-        .ce_CellForItemAtIndexPath { (collectionView, indexPath) -> UICollectionViewCell in
+        .ce_CellForItemAtIndexPath { [weak self] (collectionView, indexPath) -> UICollectionViewCell in
             let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MYCELL", forIndexPath: indexPath) as! ImageCollectionViewCell
-            
-            let message = UserInfo.shared.unreadMessages[indexPath.item]
+            let message = self!.unreadMessages[indexPath.item]
             setUnreadMessageToImage(message, cell.urlImageView, 2.0)
-//            cell.backgroundColor = UIColor.redColor()
-//            cell.urlImageView.image = UIImage(named: "imagePlaceholder")
             return cell
         }
-        .ce_DidSelectItemAtIndexPath { (collectionView, indexPath) -> Void in
-            
+        .ce_DidSelectItemAtIndexPath { [weak self] (collectionView, indexPath) -> Void in
+            let message = self!.unreadMessages.removeAtIndex(indexPath.item)
+            self!.showUnreadMessage(message)
+//            self!.collectionView.deleteItemsAtIndexPaths([indexPath])
         }
         .ce_LayoutSizeForItemAtIndexPath { (collectionView, collectionViewLayout, indexPath) -> CGSize in
             return CGSize(width: CELL_WIDTH, height: CELL_HEIGHT)
@@ -84,8 +164,33 @@ class MessagesViewController: UIViewController {
         }
     }
     
-    func showMessage() {
+    private func reloadUnreadMessages() {
+        unreadMessages = UserInfo.shared.unreadMessages
+        collectionView.reloadData()
+    }
+    
+    private func showUnreadMessage(message: UnreadMessageModel) {
+        currentDisplayMessage = message;
         
+        wordView.hidden = false
+        wordView.message = message
+        wordSendTimeLabel.hidden = false
+        wordSendTimeLabel.text = message.message.createTime.stringWithFormat("HH:mm")
+        playButton.hidden = message.message.type != .Word || message.word?.audioUrl == nil
+        forwardButton.hidden = message.message.type != .Word
+        
+//        ServerHelper.messageSetRead(message.message.id, completionHandler: { (ret, error) -> Void in
+//            if let error = error {
+//                println(error)
+//                return
+//            }
+//            UserInfo.shared.removeUnreadMessage(message.message.id)
+//            UserInfo.shared.updateFriends()
+//        })
+    }
+    
+    deinit {
+        ce_removeObserver()
     }
     
     /*
