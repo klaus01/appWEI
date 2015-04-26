fde00f573e16d7a0af4e40401735b3b3c3db023b
klaus01 <kelei0017@gmail.com>
2015-04-26 19:38:22 +0800

    增加LoadingCollectionViewCell

---
+++ appWEI/General/View/LoadingCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingCollectionViewCell.swift
+//  appWEI
+//
+//  Created by kelei on 15/4/26.
+//  Copyright (c) 2015年 kelei. All rights reserved.
+//
+
+import UIKit
+
+class LoadingCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
