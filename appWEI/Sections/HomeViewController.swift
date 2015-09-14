dc9facb84d252a7feb634d5b57250d880d5290e4
柯磊 <kelei0017@gmail.com>
2015-09-15 00:10:36 +0800

    完成首页样式

--- appWEI/Sections/HomeViewController.swift
+++ appWEI/Sections/HomeViewController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myButton.backgroundColor = THEME_BAR_COLOR
+        
+        avatarImageView.layer.borderColor = UIColor.whiteColor().CGColor
+        avatarImageView.layer.borderWidth = 4
+        
+        nicknameLabel.text = "我";
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        avatarImageView.layer.cornerRadius = avatarImageView.bounds.size.width * 0.5
     }
     
     override func viewWillAppear(animated: Bool) {
